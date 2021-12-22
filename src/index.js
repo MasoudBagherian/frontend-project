66d04f507742c5bf236f30a2cd6a3227af78f2f7
Masoud Bagherian <masoud90bagherian@gmail.com>
2021-12-23 01:40:08 +0330

    remove extra code lines in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-const root = document.getElementById('root');
-
-const h1 = React.createElement(
-  'h1',
-  { children: 'hello world' },
-  'hello world'
+ReactDOM.render(
+  App({ className: 'font-primary' }),
+  document.getElementById('root')
 );
-ReactDOM.render(App({ className: 'font-primary' }), root);
